7833c87ec7daa04322b690271151b5598f163980
Lukas_eepy <lukaseepydev@gmail.com>
2025-08-20 18:42:26 +0200

    ahhhh

--- json_edit.py
+++ json_edit.py
@@ -1,5 +1,9 @@
 import os, json
 
-sounds = os.listdir("sounds")
+sound_dir = "sounds"
+files = [f for f in os.listdir(sound_dir) if os.path.isfile(os.path.join(sound_dir, f))]
+
 with open("sounds.json", "w") as f:
-    json.dump(sounds, f, indent=2)
+    json.dump(files, f, indent=2)
+
+print(f"Generated sounds.json with {len(files)} entries.")
